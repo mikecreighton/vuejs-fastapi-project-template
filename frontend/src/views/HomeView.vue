8bbee5eb857f67a052f6f96421e8a58137361e12
Mike Creighton <mike@mikecreighton.com>
2024-11-05 09:53:14 -0800

    Updating home screen and global css.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from "vue"
-console.log("HomeView")
+console.log("HomeView :: Hello world 👋🏼")
 </script>
 
 <template>
-  <div>
-    <h1>Vue.js FastAPI Starter Project Template</h1>
-    <p>This is a simple project template for a Vue.js and FastAPI project, set up to be a monorepo.</p>
-    <p>This template is intended to be used for prototypes that use generative AI models. Take a look at the README.md file for more information.</p>
+  <div class="p-12 w-full max-w-screen-sm mx-auto">
+    <h1 class="text-2xl font-bold text-white mb-8">Vue.js FastAPI Starter Project Template</h1>
+    <p class="mb-4">This is a simple project template for a Vue.js and FastAPI project, set up to be a monorepo.</p>
+    <p class="mb-4">This template is intended to be used for prototypes that use generative AI models. Take a look at the README.md file for more information.</p>
   </div>
 </template>
 
