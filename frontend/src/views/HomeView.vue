334597e85f9525b66ec56faa75c8c23c7ae5f93a
Mike Creighton <mike@mikecreighton.com>
2024-11-05 09:30:30 -0800

    Updating README and other niceties for the project.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,14 @@
 <script setup>
-import { ref } from 'vue'
-console.log('HomeView')
-
+import { ref } from "vue"
+console.log("HomeView")
 </script>
 
 <template>
   <div>
-    <h1>Home</h1>
-    <p>Welcome to the home page.</p>
+    <h1>Vue.js FastAPI Starter Project Template</h1>
+    <p>This is a simple project template for a Vue.js and FastAPI project, set up to be a monorepo.</p>
+    <p>This template is intended to be used for prototypes that use generative AI models. Take a look at the README.md file for more information.</p>
   </div>
 </template>
 
-<style scoped lang="postcss">
-
-</style>
+<style scoped lang="postcss"></style>
